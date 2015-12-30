a4e67f26f1e42903a08776bbdaa0b784eec8db00
chenlijun <332065560@qq.com>
2015-12-30 20:47:13 +0800

    高亮显示

--- jeesite/src/main/java/com/thinkgem/jeesite/common/utils/SolrApi.java
+++ jeesite/src/main/java/com/thinkgem/jeesite/common/utils/SolrApi.java
@@ -24,12 +24,14 @@ import org.springframework.stereotype.Component;
 
 import com.thinkgem.jeesite.modules.cms.dao.ArticleDao;
 import com.thinkgem.jeesite.modules.cms.entity.Article;
+
 /**
  * 分词工具类
+ * 
  * @author chenlijun08@163.com
-
+ * 
  * @version 1.0
-
+ * 
  * @date 2015年12月19日
  */
 @Component
@@ -52,21 +54,18 @@ public class SolrApi {
 		client.commit();
 	}
 
-	public void deleteDoc(String... ids) throws SolrServerException,
-			IOException {
+	public void deleteDoc(String... ids) throws SolrServerException, IOException {
 		for (String id : ids) {
 			client.deleteById(id);
 		}
 		client.commit(true, true);
 	}
 
-	public SolrDocument getDocById(String id) throws SolrServerException,
-			IOException {
+	public SolrDocument getDocById(String id) throws SolrServerException, IOException {
 		return client.getById(id);
 	}
 
-	public List<ContentBean> queryAll(int startIndex, int pageSize)
-			throws SolrServerException, IOException {
+	public List<ContentBean> queryAll(int startIndex, int pageSize) throws SolrServerException, IOException {
 		List<ContentBean> result = new ArrayList<ContentBean>();
 		ModifiableSolrParams params = new ModifiableSolrParams();
 		// 查询关键词，*:*代表所有属性、所有值，即所有index
@@ -82,18 +81,13 @@ public class SolrApi {
 		if (list != null && list.size() > 0) {
 			for (SolrDocument d : list) {
 				ContentBean bean = new ContentBean();
-				bean.setContent(d.getFieldValue("content") == null ? "" : d
-						.getFieldValue("content").toString());
-				bean.setDescription(d.getFieldValue("description") == null ? ""
-						: d.getFieldValue("description").toString());
-				bean.setId(d.getFieldValue("id") == null ? "" : d
-						.getFieldValue("id").toString());
-				bean.setKeywords(d.getFieldValue("keywords") == null ? "" : d
-						.getFieldValue("keywords").toString());
-				bean.setTitle(d.getFieldValue("title") == null ? "" : d
-						.getFieldValue("title").toString());
-				bean.setTitle(d.getFieldValue("create_date") == null ? "" : d
-						.getFieldValue("create_date").toString());
+				bean.setContent(d.getFieldValue("content") == null ? "" : d.getFieldValue("content").toString());
+				bean.setDescription(d.getFieldValue("description") == null ? "" : d.getFieldValue("description")
+						.toString());
+				bean.setId(d.getFieldValue("id") == null ? "" : d.getFieldValue("id").toString());
+				bean.setKeywords(d.getFieldValue("keywords") == null ? "" : d.getFieldValue("keywords").toString());
+				bean.setTitle(d.getFieldValue("title") == null ? "" : d.getFieldValue("title").toString());
+				bean.setTitle(d.getFieldValue("create_date") == null ? "" : d.getFieldValue("create_date").toString());
 				result.add(bean);
 			}
 		}
@@ -101,49 +95,51 @@ public class SolrApi {
 
 	}
 
-	public Map<String, Object> queryByParam(String condition, int startIndex,
-			int PageSize) throws SolrServerException, IOException {
+	public Map<String, Object> queryByParam(String condition, int startIndex, int PageSize) throws SolrServerException,
+			IOException {
 		Map<String, Object> result = new HashMap<String, Object>();
 		result.put("rows", 0L);
-		SolrQuery params = new SolrQuery("title:" + condition + " OR "
-				+ "description:" + condition + " OR " + "keywords:" + condition
-				+ " OR " + "content:" + condition);
+		SolrQuery params = new SolrQuery("title:" + condition + " OR " + "description:" + condition + " OR "
+				+ "keywords:" + condition + " OR " + "content:" + condition);
 		params.addSort("title", ORDER.asc);
 		// 分页：start开始页，rows每页显示记录条数
 		params.setStart(startIndex);
 		params.setRows(PageSize);
 		// 设置高亮
 		params.setHighlight(true); // 开启高亮组件
+		params.setHighlightFragsize(10);
+		params.setHighlightRequireFieldMatch(true);
 		params.addHighlightField("title");// 高亮字段
 		params.addHighlightField("keywords");// 高亮字段
 		params.addHighlightField("description");// 高亮字段
+		params.addHighlightField("content");// 高亮字段
 		params.setHighlightSimplePre("<font color='red'>");// 标记，高亮关键字前缀
 		params.setHighlightSimplePost("</font>");// 后缀
-		params.setHighlightSnippets(1);// 结果分片数，默认为1
-		SolrDocumentList list = client.query(params).getResults();
+		QueryResponse response = client.query(params);
+		SolrDocumentList list = response.getResults();
+		Map<String, Map<String, List<String>>> lightMap = response.getHighlighting();
 		if (list != null && list.size() > 0) {
 			List<ContentBean> datalist = new ArrayList<ContentBean>();
-			if("*".equals(condition)){
-				condition="";
+			if ("*".equals(condition)) {
+				condition = "";
 			}
 			for (SolrDocument d : list) {
 				ContentBean bean = new ContentBean();
-				bean.setContent(d.getFieldValue("content") == null ? "" : d
-						.getFieldValue("content").toString().replaceAll(condition, "<font color='red'>"+condition+"</font>"));
-				bean.setDescription(d.getFieldValue("description") == null ? ""
-						: d.getFieldValue("description").toString().replaceAll(condition, "<font color='red'>"+condition+"</font>"));
-				bean.setId(d.getFieldValue("id") == null ? "" : d
-						.getFieldValue("id").toString());
-				bean.setKeywords(d.getFieldValue("keywords") == null ? "" : d
-						.getFieldValue("keywords").toString().replaceAll(condition, "<font color='red'>"+condition+"</font>"));
-				bean.setTitle(d.getFieldValue("title") == null ? "" : d
-						.getFieldValue("title").toString());
-				bean.setCreate_date(d.getFieldValue("create_date") == null ? null
-						: (Date)d.getFieldValue("create_date"));
-				Article article = articleDao.get(d.getFieldValue("id")
-						.toString());
+				Map<String, List<String>> map = lightMap.get(d.getFieldValue("id").toString());
+				bean.setContent((map.get("content") == null || "".equals(condition)) ? d.getFieldValue("content")
+						.toString() : map.get("content").get(0));
+				bean.setDescription((map.get("description") == null || "".equals(condition)) ? d.getFieldValue(
+						"description").toString() : map.get("description").get(0));
+				bean.setId(d.getFieldValue("id").toString());
+				bean.setKeywords((map.get("keywords") == null || "".equals(condition)) ? d.getFieldValue("keywords")
+						.toString() : map.get("keywords").get(0));
+				bean.setTitle((map.get("title") == null || "".equals(condition)) ? d.getFieldValue("title").toString()
+						: map.get("title").get(0));
+				bean.setCreate_date(d.getFieldValue("create_date") == null ? null : (Date) d
+						.getFieldValue("create_date"));
+				Article article = articleDao.get(d.getFieldValue("id").toString());
 				bean.setHits(article == null ? 0 : article.getHits());
-				bean.setCategory(article.getCategory()==null?null:article.getCategory());
+				bean.setCategory(article.getCategory() == null ? null : article.getCategory());
 				datalist.add(bean);
 			}
 			result.put("rows", list.getNumFound());
@@ -153,17 +149,14 @@ public class SolrApi {
 
 	}
 
-	public List<Map<String, Long>> getKeyWordsCount()
-			throws SolrServerException, IOException {
+	public List<Map<String, Long>> getKeyWordsCount() throws SolrServerException, IOException {
 		List<Map<String, Long>> result = new ArrayList<Map<String, Long>>();
 		SolrQuery params = new SolrQuery("*:*");
 		params.addSort("title", ORDER.asc);
 		params.setStart(0);
 		params.setRows(Integer.MAX_VALUE);
-		params.setFacet(true).setQuery("*:*").setFacetMinCount(1)
-				.setFacetLimit(5).addFacetField("title")
-				.addFacetField("keywords").addFacetField("content")
-				.addFacetField("description");// 分片字段
+		params.setFacet(true).setQuery("*:*").setFacetMinCount(1).setFacetLimit(5).addFacetField("title")
+				.addFacetField("keywords").addFacetField("content").addFacetField("description");// 分片字段
 		QueryResponse response = client.query(params);
 		// 输出分片信息
 		List<FacetField> facets = response.getFacetFields();
